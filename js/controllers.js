f138a08067e70ca69719006e4ca5b595bdc1e4ea
Alan Mosko <moskoweb@gmail.com>
2015-03-17 19:31:00 -0300

    First Commit - Add Files

---
+++ js/controllers.js
@@ -0,0 +1,67 @@
+var App = angular.module('App', ['ngRoute','ionic']);
+
+App.config(function ($routeProvider) {
+
+  $routeProvider
+  .when('/', {
+    templateUrl: 'templates/home.html',
+    controller: 'HomeCtrl'
+  })
+  .when('/semana', {
+    templateUrl: 'templates/semana.html',
+    controller: 'SemanaCtrl'
+  })
+  .when('/aula/:id', {
+    templateUrl: 'templates/aula.html',
+    controller: 'AulaCtrl'
+  })
+  .when('/sobre', {
+    templateUrl: 'templates/sobre.html',
+    controller: 'SobreCtrl'
+  })
+  .otherwise({ redirectTo: '/' })
+
+})
+
+App.controller('HomeCtrl', function($rootScope, $location) {
+   $rootScope.activetab = $location.path();
+});
+App.controller('AulaCtrl', function($rootScope, $location) {
+   $rootScope.activetab = $location.path();
+});
+App.controller('SobreCtrl', function($rootScope, $location) {
+   $rootScope.activetab = $location.path();
+});
+App.controller('SemanaCtrl', function($rootScope, $location) {
+   $rootScope.activetab = $location.path();
+});
+
+// Faz Leitura do JSON
+App.controller("AulaCtrl", function($scope, $http) {
+  $http.get('../db/db.json').
+    success(function(data, status, headers, config) {
+      $scope.posts = data;
+    }).
+    error(function(data, status, headers, config) {
+      // log error
+    });
+});
+// Fim Leitura do JSON
+
+// Diretivas
+// App.directive('dragBack', function($ionicGesture, $state) {
+//   return {
+//     restrict : 'A',
+//     link : function(scope, elem, attr) {
+
+//       $ionicGesture.on('swiperight', function(event) {
+
+//         console.log('Got swiped!');
+//         event.preventDefault();
+//         window.history.back();
+
+//       }, elem);
+
+//     }
+//   }
+// })
