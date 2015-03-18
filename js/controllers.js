a4554aae16c0be05e4d0c03a64638dba5fd872fa
Alan Mosko <moskoweb@gmail.com>
2015-03-18 18:17:51 -0300

    Finalização do App

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,6 @@
 var App = angular.module('App', ['ngRoute','ionic']);
 
-App.config(function ($routeProvider) {
+App.config(function ($routeProvider, $locationProvider) {
 
   $routeProvider
   .when('/', {
@@ -11,16 +11,33 @@ App.config(function ($routeProvider) {
     templateUrl: 'templates/semana.html',
     controller: 'SemanaCtrl'
   })
-  .when('/aula/:id', {
-    templateUrl: 'templates/aula.html',
+  .when('/aula1', {
+    templateUrl: 'templates/aula1.html',
+    controller: 'AulaCtrl'
+  })
+  .when('/aula2', {
+    templateUrl: 'templates/aula2.html',
+    controller: 'AulaCtrl'
+  })
+  .when('/aula3', {
+    templateUrl: 'templates/aula3.html',
+    controller: 'AulaCtrl'
+  })
+  .when('/aula4', {
+    templateUrl: 'templates/aula4.html',
+    controller: 'AulaCtrl'
+  })
+  .when('/aula5', {
+    templateUrl: 'templates/aula5.html',
     controller: 'AulaCtrl'
   })
   .when('/sobre/', {
     templateUrl: 'templates/sobre.html',
     controller: 'SobreCtrl'
   })
-  .otherwise({ redirectTo: '/' })
+  .otherwise({ redirectTo: '/' });
 
+  $locationProvider.html5Mode(true);
 })
 
 // Controller de Rota
